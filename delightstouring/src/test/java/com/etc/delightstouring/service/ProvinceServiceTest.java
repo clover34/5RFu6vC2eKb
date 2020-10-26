f676e3304a474bb1f100008c8ef5260759c2c581
17608994857 <17608994857@zrgj.com>
2020-10-26 21:41:10 +0800

    羊贵周

---
+++ delightstouring/src/test/java/com/etc/delightstouring/service/ProvinceServiceTest.java
@@ -0,0 +1,20 @@
+package com.etc.delightstouring.service;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ClassName ProvinceService
+ * @Description TODO
+ * @Author Administrator
+ * @Date 20/10/26 19:14
+ * @Version 1.0
+ **/
+public class ProvinceServiceTest {
+    @Test
+    public void testfindAll(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ProvinceService provinceService = context.getBean("provinceServiceImpl", ProvinceService.class);
+        System.out.println("provinceService.findAllProvince() = " + provinceService.findAllProvince());
+    }
+}
